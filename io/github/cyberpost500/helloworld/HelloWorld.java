96727a450ec113a0b14a387e8e460b2c3ef232f9
Stephen <stephenrd@gmail.com>
2017-01-23 23:07:42 -0500

    reorganized to reflect how packages are organized in the professional world

---
+++ io/github/cyberpost500/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package io.github.cyberpost500.helloworld;
+
+/**
+ * This is a very simple hello-world style application that just outputs a
+ * greeting.
+ */
+public class HelloWorld {
+    /**
+     * Entry point of the application.
+     * @param args Unused, but required.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ io/github/cyberpost500/helloworld/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This is a convenient place to put the hello world application, but that's
+ * all it contains.
+ */
+package io.github.cyberpost500.helloworld;
