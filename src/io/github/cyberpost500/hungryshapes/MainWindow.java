153f29cb5b6028bd40f52214bc19f1662827177b
Stephen <stephenrd@gmail.com>
2017-01-23 23:19:05 -0500

    moved the frame creation out of main and into the constructor added a graphical hello world message

--- src/io/github/cyberpost500/hungryshapes/MainWindow.java
+++ src/io/github/cyberpost500/hungryshapes/MainWindow.java
@@ -1,6 +1,10 @@
 package io.github.cyberpost500.hungryshapes;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Font;
 /**
  * This class defines a very simple JFrame that will later become the game's
  * main window.
@@ -9,10 +13,28 @@ import javax.swing.JFrame;
 public class MainWindow extends JFrame {
     private final static String APP_TITLE = "Hungry Shapes";
 
+    /**
+     * Creates a MainWindow that's 800x600 and will cause the app to exit if it
+     * is closed.
+     */
+    public MainWindow() {
+        super(APP_TITLE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+    }
+
     public static void main(String[] args) {
-        final JFrame frame = new JFrame(APP_TITLE);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800,600);
-        frame.setVisible(true);
+        final MainWindow window = new MainWindow();
+        window.getContentPane().add(new JPanel(true) {
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setFont(new Font("Default", Font.PLAIN, 32));
+                g.setColor(Color.BLUE);
+                g.drawString("Hello, World!", 10, 40);
+            }
+        });
+
+        window.setVisible(true);
     }
 }
